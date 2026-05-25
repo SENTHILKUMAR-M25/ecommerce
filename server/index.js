8b99d1e72072b5d607f480edcc7731046130c92e
muthu <muthuvenkadesh49@gmail.com>
2026-05-25 20:21:32 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "https://ecommerce-eight-virid-50.vercel.app",
       // "https://ecommerce-eight-virid-50.vercel.app",
 
     ],
